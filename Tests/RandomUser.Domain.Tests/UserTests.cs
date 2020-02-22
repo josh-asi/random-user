6e640d000f9d00dcade9f97e4c2afb049bb08e48
Josh <josh.asi@hotmail.co.nz>
2020-02-22 19:05:19 +1300

    Tested updating name for User domain

--- Tests/RandomUser.Domain.Tests/UserTests.cs
+++ Tests/RandomUser.Domain.Tests/UserTests.cs
@@ -36,5 +36,27 @@ namespace RandomUser.Domain.Tests
 
             Assert.Throws<InvalidFormatException>(() => new User.User(id, name, dob, phoneNo, email, image));
         }
+
+        [Fact]
+        public void User_Name_Should_Be_Updatable()
+        {
+            var id = 1;
+            var name = new Name(Title.MR, "John", "Doe");
+            var dob = DateTime.Now;
+            var phoneNo = new PhoneNumber(1234567);
+            var image = new Image("some-image");
+            var email = new Email("john.doe@email.com");
+
+            var user = new User.User(id, name, dob, phoneNo, email, image);
+
+            var newName = new Name(Title.MS, "Jane", "Doe");
+
+            user.UpdateName(newName);
+
+            Assert.Equal(newName.Title, user.Name.Title);
+            Assert.Equal(newName.FirstName, user.Name.FirstName);
+            Assert.Equal(newName.LastName, user.Name.LastName);
+
+        }
     }
 }
