3fc5e964da9ee6f51484077f908fdd09b79bd0bc
Josh <josh.asi@hotmail.co.nz>
2020-02-22 17:16:31 +1300

    Created a use case for updating names

---
+++ src/RandomUser.Application/Commands/UpdateUser/UpdateUserUseCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using RandomUser.Application.Repository;
+using RandomUser.Domain.User;
+
+namespace RandomUser.Application.Commands.UpdateUser
+{
+    public class UpdateUserUseCase : IUpdateUserUseCase
+    {
+        private readonly IUserWriteOnlyRepository userWriteOnlyRepository;
+        private readonly IUserReadOnlyRepository userReadOnlyRepository;
+
+        public UpdateUserUseCase(IUserWriteOnlyRepository userWriteOnlyRepository, IUserReadOnlyRepository userReadOnlyRepository)
+        {
+            this.userWriteOnlyRepository = userWriteOnlyRepository;
+            this.userReadOnlyRepository = userReadOnlyRepository;
+        }
+
+        public async Task<User> Execute(int userId, Name newName)
+        {
+            var user = await userReadOnlyRepository.Get(userId);
+            if (user == null) throw new NotFoundException("User not found");
+
+            user.UpdateName(newName);
+
+            await userWriteOnlyRepository.UpdateAsync(user);
+
+            return user;
+        }
+    }
+}
