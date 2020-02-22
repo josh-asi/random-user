709bc693a12bec94d32a61d86f0a866a440caa38
Josh <josh.asi@hotmail.co.nz>
2020-02-22 19:48:39 +1300

    Refactored GenerateUsers to a static class. Tested UpdateUserUseCase

---
+++ Tests/RandomUser.Application.Tests/CommandTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using RandomUser.Application.Queries;
+using Xunit;
+using NSubstitute;
+using RandomUser.Domain.User;
+using RandomUser.Domain.User.ValueObjects;
+using System.Collections.Generic;
+using RandomUser.Application.Repository;
+using RandomUser.Application.Commands.UpdateUser;
+
+namespace RandomUser.Application.Tests
+{
+    public class CommandTests
+    {
+        public readonly IUserWriteOnlyRepository userWriteOnlyRepository;
+        public readonly IUserReadOnlyRepository userReadOnlyRepository;
+
+        public readonly List<User> users;
+
+        public CommandTests()
+        {
+            userWriteOnlyRepository = Substitute.For<IUserWriteOnlyRepository>();
+            userReadOnlyRepository = Substitute.For<IUserReadOnlyRepository>();
+            users = UserGeneration.Generate();
+        }
+
+        [Fact]
+        public async void Update_Name()
+        {
+            var userId = 1;
+            var newName = new Name(Title.MS, "Jane", "Doe");
+
+            userReadOnlyRepository.Get(userId).Returns(users[0]);
+
+            var updateNameUseCase = new UpdateUserUseCase(userWriteOnlyRepository, userReadOnlyRepository);
+
+            var result = await updateNameUseCase.Execute(userId, newName);
+
+            Assert.Equal(newName.FullName, result.Name.FullName);
+        }
+    }
+}
--- Tests/RandomUser.Application.Tests/QueryTests.cs
+++ Tests/RandomUser.Application.Tests/QueryTests.cs
@@ -17,7 +17,7 @@ namespace RandomUser.Application.Tests
         public QueryTests()
         {
             userQuery = Substitute.For<IUserQuery>();
-            users = GenerateUsers();
+            users = UserGeneration.Generate();
         }
 
 
@@ -95,57 +95,6 @@ namespace RandomUser.Application.Tests
 
             Assert.Equal(filteredUsers, result);
             Assert.Equal(filteredUsers[0].Name.LastName, result[0].Name.LastName);
-        }
-
-
-        private List<User> GenerateUsers()
-        {
-            var users = new List<User>();
-
-            var id = 1;
-            var name = new Name("Mr", "John", "Doe");
-            var dob = DateTime.Now;
-            var phone = new PhoneNumber(1234567);
-            var email = new Email("john.doe@email.com");
-            var image = new Image("default-image.jpg");
-            users.Add(new User(id, name, dob, phone, email, image));
-
-            id = 2;
-            name = new Name("Ms", "John", "Donor");
-            dob = DateTime.Now;
-            phone = new PhoneNumber(1234568);
-            email = new Email("john.donor@email.com");
-            users.Add(new User(id, name, dob, phone, email, image));
-
-            id = 3;
-            name = new Name("Mr", "Jane", "Doe");
-            dob = DateTime.Now;
-            phone = new PhoneNumber(1234569);
-            email = new Email("jane.doe@email.com");
-            users.Add(new User(id, name, dob, phone, email, image));
-
-            id = 4;
-            name = new Name("Mr", "Bob", "Dylan");
-            dob = DateTime.Now;
-            phone = new PhoneNumber(8673957);
-            email = new Email("bob.dylan@email.com");
-            users.Add(new User(id, name, dob, phone, email, image));
-
-            id = 5;
-            name = new Name("Mr", "Robert", "Love");
-            dob = DateTime.Now;
-            phone = new PhoneNumber(7592648);
-            email = new Email("robert.love@email.com");
-            users.Add(new User(id, name, dob, phone, email, image));
-
-            id = 6;
-            name = new Name("Ms", "Daffey", "Duck");
-            dob = DateTime.Now;
-            phone = new PhoneNumber(6392759);
-            email = new Email("daffey.duck@email.com");
-            users.Add(new User(id, name, dob, phone, email, image));
-
-            return users;
         }
     }
 }
